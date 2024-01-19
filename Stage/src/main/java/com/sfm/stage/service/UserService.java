7bf34ed08d317657a918510853c30f9a3a29cfb5
unknown <adembenkahla02bk@gmail.com>
2024-01-19 15:26:56 +0100

    first commit

---
+++ Stage/src/main/java/com/sfm/stage/service/UserService.java
@@ -0,0 +1,16 @@
+package com.sfm.stage.service;
+
+
+import java.util.List;
+
+import com.sfm.stage.dto.UserDto;
+import com.sfm.stage.entity.User;
+
+public interface UserService {
+    void saveUser(UserDto userDto);
+
+    User findUserByEmail(String email);
+
+    List<UserDto> findAllUsers();
+    void deleteById(long id);
+}
